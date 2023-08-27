6b007264a1b19be70643278eed6dc622636fd807
DL William <dlprogrammer18@gmail.com>
2023-08-27 17:13:36 +0800

    chore: update file class functions

--- controllers/file.py
+++ controllers/file.py
@@ -5,12 +5,18 @@ class File:
     def __init__(self, filename: str, sheetname: str):
         self.filename = filename
         self.sheetname = sheetname
+        self._sheet = self.__load_sheet()
 
     def __load_sheet(self) -> any:
         file = file_helper.OpenPyxlReadExcel(self.filename)
         sheet = file[self.sheetname]
         return sheet
+    
+    def loaded_sheet(self) -> any:
+        return self._sheet
 
     def get_sheet_value(self, cellname: str) -> any:
-        sheet = self.__load_sheet()
-        return sheet[cellname].value
+        return self._sheet[cellname].value
+    
+    def get_max_rows(self) -> int:
+        return self._sheet.max_row
