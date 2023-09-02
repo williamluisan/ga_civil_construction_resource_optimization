6ecf862b294d35c287820f38ba1fc06cfbf4121b
DL William <dlprogrammer18@gmail.com>
2023-09-02 17:28:51 +0800

    feat: (WIP) fitness function

--- modules/main.py
+++ modules/main.py
@@ -14,11 +14,13 @@ class Main:
 
         # create random solutions
         random_solutions = RandomSolutions(first_individual)
-        random_solutions.create_random_solutions()
+        first_solution = random_solutions.create_random_solutions()
 
-        # calculate fitness function
-        
+        # GA loop starts (to find the best solution, then stop)
+        solution = first_solution
+        for v_solution in solution:
+            print(solution[v_solution], end = "\n\n")
 
         # (debug)
-        pretty_print = json.dumps(random_solutions.create_random_solutions(), indent = 4)
-        print(pretty_print)
+        # pretty_print = json.dumps(random_solutions.create_random_solutions(), indent = 4)
+        # print(pretty_print)
