5bd91bc2ec47757c1ab081e06e558cf6f579b925
DL William <dlprogrammer18@gmail.com>
2023-09-12 22:13:20 +0800

    chore: preparing main cmd for pygad arguments

--- cmd/app.py
+++ cmd/app.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import textwrap
 
 # Add the path to the parent directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -8,7 +9,33 @@ from modules.main import Main
 from modules.pygad import *
 
 def main():
-    Main.execute()
+    argv = sys.argv
+
+    argv_passed = False
+    argv_second = None
+    if len(argv) == 1 or len(argv) == 2:
+        argv_passed = True
+        if len(argv) == 2:
+            if argv[1] == 'pygad':
+                argv_second = argv[1]
+            else:
+                argv_passed = False
+
+    if argv_passed == True:
+        if argv_second == 'pygad':
+            print("run pygad")
+            # run pygad here ..
+        else:
+            Main.execute()
+
+    if argv_passed == False:
+        message = """
+            Usage: python cmd/app.py <options>\n
+            Options
+                - (empty)\t: using original tailored algorithm by the author
+                - pygad\t: using PyGad library
+        """
+        print(textwrap.dedent(message).strip())
 
 if __name__ == "__main__":
     main()
